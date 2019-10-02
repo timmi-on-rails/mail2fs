8f05af31577ad8fc28e37c857c66f7b8d524267e
Tom Scheler <dd-tom@web.de>
2019-10-02 23:04:01 +0200

    next try

--- vsto/build_vsto_pack.bat
+++ vsto/build_vsto_pack.bat
@@ -4,8 +4,9 @@ set ARCHIVE=vsto-pack.zip
 del "%ARCHIVE%"
 "%ZIP_EXE%" a "%ARCHIVE%" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.targets"
 "%ZIP_EXE%" rn "%ARCHIVE%" "Microsoft.VisualStudio.Tools.Office.targets" "Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.targets"
-exit
-"%ZIP_EXE%" a vsto-pack.zip "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.targets"
+
+"%ZIP_EXE%" a "%ARCHIVE%" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\ReferenceAssemblies\v4.0\Microsoft.Office.Tools.Common.v4.0.Utilities.dll"
+"%ZIP_EXE%" rn "%ARCHIVE%" "Microsoft.Office.Tools.Common.v4.0.Utilities.dll" "Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\ReferenceAssemblies\v4.0\Microsoft.Office.Tools.Common.v4.0.Utilities.dll"
 exit
 
 "%7Z_EXE%" a vsto-pack.zip "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Tools.Applications.BuildTasks.dll"
