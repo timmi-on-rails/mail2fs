2fc26540597be192942e094f4c3d6f0d7c3b6ab5
Tom Scheler <dd-tom@web.de>
2019-10-02 23:42:39 +0200

    next try

--- vsto/build_vsto_pack.bat
+++ vsto/build_vsto_pack.bat
@@ -5,6 +5,9 @@ del "%ARCHIVE%"
 "%ZIP_EXE%" a "%ARCHIVE%" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.targets"
 "%ZIP_EXE%" rn "%ARCHIVE%" "Microsoft.VisualStudio.Tools.Office.targets" "Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.targets"
 
+"%ZIP_EXE%" a "%ARCHIVE%" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.BuildTasks.dll"
+"%ZIP_EXE%" rn "%ARCHIVE%" "Microsoft.VisualStudio.Tools.Office.BuildTasks.dll" "Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\v16.0\OfficeTools\Microsoft.VisualStudio.Tools.Office.BuildTasks.dll"
+
 "%ZIP_EXE%" a "%ARCHIVE%" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\ReferenceAssemblies\v4.0\Microsoft.Office.Tools.Common.v4.0.Utilities.dll"
 "%ZIP_EXE%" rn "%ARCHIVE%" "Microsoft.Office.Tools.Common.v4.0.Utilities.dll" "Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\ReferenceAssemblies\v4.0\Microsoft.Office.Tools.Common.v4.0.Utilities.dll"
 
