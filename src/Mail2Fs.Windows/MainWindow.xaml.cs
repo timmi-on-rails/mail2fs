c958989b9d3471f82f3e365421fb06f707df1e7c
Tom Scheler <dd-tom@web.de>
2019-10-03 13:28:38 +0200

    implemented minimize to tray

--- src/Mail2Fs.Windows/MainWindow.xaml.cs
+++ src/Mail2Fs.Windows/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Mail2Fs.Windows
 {
@@ -20,9 +8,60 @@ namespace Mail2Fs.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool quitting = false;
+        System.Windows.Forms.NotifyIcon notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.BalloonTipText = "mail2fs has been minimised. Click the tray icon to show.";
+            notifyIcon.BalloonTipTitle = "mail2fs";
+            notifyIcon.Text = "mail2fs";
+            notifyIcon.Icon = Mail2FsResources.icon;
+
+            var menuItem = new System.Windows.Forms.MenuItem
+            {
+                Text = "Quit",
+            };
+            menuItem.Click += MenuItem_Click;
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(new[] {
+                menuItem
+            });
+
+            notifyIcon.Click += NotifyIcon_Click;
+            Closed += MainWindow_Closed;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            quitting = true;
+            Close();
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            notifyIcon.Visible = false;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!quitting)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(1000);
+                e.Cancel = true;
+            }
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            notifyIcon.Dispose();
+            notifyIcon = null;
         }
     }
 }
